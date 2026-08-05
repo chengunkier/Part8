58fba665916515b3267433194a8054ce7c9814ac
chengunkier <inabamicheal97@gmail.com>
2026-08-05 09:33:07 +0300

    completed exercise 3. All authors

--- fs-graphql/library-backend/index.js
+++ fs-graphql/library-backend/index.js
@@ -93,12 +93,14 @@ const typeDefs = `
     name: String!
     id: ID!
     born: Int
+    bookCount: Int!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -107,6 +109,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length,
   }
 }
 
