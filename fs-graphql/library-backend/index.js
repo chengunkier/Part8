3a2464698efecabb95b5fdb7d00b99a899bd2fab
chengunkier <inabamicheal97@gmail.com>
2026-08-05 10:32:42 +0300

    completed exercise 7. Updating the birth year of an author

--- fs-graphql/library-backend/index.js
+++ fs-graphql/library-backend/index.js
@@ -110,6 +110,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -146,6 +150,16 @@ const resolvers = {
       }
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
