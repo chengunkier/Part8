732d0fc8cc8080b881efe23756fc1d6401acf108
chengunkier <inabamicheal97@gmail.com>
2026-08-13 15:32:04 +0300

    completed exercise 12. Authors birth year advanced

--- fs-graphql/library-frontend/src/components/Authors.jsx
+++ fs-graphql/library-frontend/src/components/Authors.jsx
@@ -61,7 +61,10 @@ const Authors = (props) => {
       <form onSubmit={submit}>
         <div>
           name
-          <select value={name} onChange={({ target }) => setName(target.value)}>
+          <select
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          >
             <option value="">select author</option>
             {authors.map((a) => (
               <option key={a.name} value={a.name}>
