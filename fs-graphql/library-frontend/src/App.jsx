e99651e971630843cc72e12722c0ff639b6c00bb
chengunkier <inabamicheal97@gmail.com>
2026-08-18 12:01:09 +0300

    completed exercise 19. Log in

--- fs-graphql/library-frontend/src/App.jsx
+++ fs-graphql/library-frontend/src/App.jsx
@@ -1,22 +1,43 @@
 import { useState } from 'react'
+import { useApolloClient } from '@apollo/client/react'
 import Authors from './components/Authors'
 import Book from './components/Book'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(
+    localStorage.getItem('library-user-token')
+  )
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+    setPage('authors')
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {token ? (
+          <>
+            <button onClick={() => setPage('add')}>add book</button>
+            <button onClick={logout}>logout</button>
+          </>
+        ) : (
+          <button onClick={() => setPage('login')}>login</button>
+        )}
       </div>
 
       <Authors show={page === 'authors'} />
       <Book show={page === 'books'} />
       <NewBook show={page === 'add'} />
+      <LoginForm show={page === 'login'} setToken={setToken} />
     </div>
   )
 }
