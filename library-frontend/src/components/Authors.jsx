fcba03b1a208cfd22b9f8e3600f7690af36d9d8d
chengunkier <inabamicheal97@gmail.com>
2026-08-24 12:09:05 +0300

    completed exercise 8. Authors view

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,24 +1,23 @@
-const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
-
+const Authors = ({ authors }) => {
   return (
     <div>
-      <h2>authors</h2>
+      <h2>Authors</h2>
+
       <table>
-        <tbody>
+        <thead>
           <tr>
             <th></th>
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
-            <tr key={a.id}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+        </thead>
+
+        <tbody>
+          {authors.map((author) => (
+            <tr key={author.name}>
+              <td>{author.name}</td>
+              <td>{author.born}</td>
+              <td>{author.bookCount}</td>
             </tr>
           ))}
         </tbody>
