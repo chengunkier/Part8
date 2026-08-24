b97c6509fd12016c0cc512dbc9f145b6409133ff
chengunkier <inabamicheal97@gmail.com>
2026-08-24 14:47:54 +0300

    completed exercise 15. Database, part 3

--- library-backend/resolvers.js
+++ library-backend/resolvers.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql')
 const Book = require('./models/book')
 const Author = require('./models/author')
 
@@ -51,7 +52,20 @@ const resolvers = {
           name: args.author
         })
 
-        await author.save()
+        try {
+          await author.save()
+        } catch (error) {
+          throw new GraphQLError(
+            `Creating author failed: ${error.message}`,
+            {
+              extensions: {
+                code: 'BAD_USER_INPUT',
+                invalidArgs: args.author,
+                error
+              }
+            }
+          )
+        }
       }
 
       const book = new Book({
@@ -61,7 +75,22 @@ const resolvers = {
         genres: args.genres
       })
 
-      return book.save()
+      try {
+        await book.save()
+      } catch (error) {
+        throw new GraphQLError(
+          `Creating book failed: ${error.message}`,
+          {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.title,
+              error
+            }
+          }
+        )
+      }
+
+      return book
     },
 
     editAuthor: async (root, args) => {
@@ -75,7 +104,20 @@ const resolvers = {
 
       author.born = args.setBornTo
 
-      return author.save()
+      try {
+        return await author.save()
+      } catch (error) {
+        throw new GraphQLError(
+          `Updating author failed: ${error.message}`,
+          {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.name,
+              error
+            }
+          }
+        )
+      }
     }
   }
 }
