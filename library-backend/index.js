4980cf5a3d87ba35b82552b65a9319c855a98198
chengunkier <inabamicheal97@gmail.com>
2026-08-24 11:38:30 +0300

    completed exercise 3. All authors

--- library-backend/index.js
+++ library-backend/index.js
@@ -88,10 +88,18 @@ const typeDefs = `
     id: ID!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -100,6 +108,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+
+  Author: {
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length,
   },
 }
 
